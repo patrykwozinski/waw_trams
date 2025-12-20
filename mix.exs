713352f40f0d682b9053935611f97cef8401d6b7
Patryk Woziński <patrykwo@monday.com>
2025-12-20 11:57:23 +0100

    chore: CI supported by GitHub Actions

--- mix.exs
+++ mix.exs
@@ -87,6 +87,7 @@ defmodule WawTrams.MixProject do
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:ex_doc, "~> 0.35", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:protobuf, "~> 0.13"},
       {:castore, "~> 1.0"},
       {:tz, "~> 0.28"}
